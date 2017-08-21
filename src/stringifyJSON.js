1e3a01e10147e9193468b80addcc180a086512e7
Hack Reactor Student <student@hackreactor.com>
2017-08-21 15:59:19 -0700

    final commit

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,59 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // cover the primitive data types
+  var isPrimitive = function (obj) {
+    if (typeof obj === 'number' || typeof obj === 'string' || typeof obj === 'boolean' || obj === null) {
+      return true;
+    } 
+    return false;
+  };
+  // determine if it can not be stringify or can
+  var isUnStringifyable = function (obj) {
+    if (obj === undefined || typeof obj === 'function') {
+      return true;
+    } 
+    return false;
+  };
+  // if it is primitive data type assign return values 
+  if (isPrimitive(obj)) {
+    if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+      return '' + obj;
+    } else if (typeof obj === 'string') {
+      return '"' + obj + '"';
+    }
+  }
+
+  // return values for undefined and function
+  if (isUnStringifyable(obj)) {
+    return undefined;
+  }
+  // utilitize recursion for objects and arrays  
+  if (Array.isArray(obj)) {
+    var arrStr = '[';
+    // some recursion that adds to arrStr
+    obj.forEach(function(item) {
+      arrStr += stringifyJSON(item);
+      if (item !== obj[obj.length - 1]) {
+        arrStr += ',';
+      }
+    });
+    
+    return arrStr + ']';
+  }
+  if (typeof obj === 'object') {
+    var objStr = '{';
+    var lastKey = Object.keys(obj).pop();
+    for (var key in obj) {
+      if (!isUnStringifyable(obj[key])) {
+        objStr += '"' + key + '":' + stringifyJSON(obj[key]);
+        if (key !== lastKey) {
+          objStr += ',';
+        }
+      }
+    }
+   
+    return objStr + '}';
+  }
 };
+
